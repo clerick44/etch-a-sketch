487be4e9d7a755373a55619af506e427c7846782
Jesse Erickson <clerick44@gmail.com>
2021-04-02 17:05:12 -0500

    final commit

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-// global variables
+const gridSpace = document.getElementById("playGrid");
+const inputButton = document.getElementById("inputConfirm");
 
-for (let i = 0; i < 16; i++) {
-  var box = document.createElement("div");
-  box.className = i;
-  document.getElementById("playGrid").appendChild(box);
+document.addEventListener("load", initializeGrid());
+inputButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  resizeGrid();
+});
+
+function initializeGrid() {
+  gridSize(16);
+  buildGrid(16);
+}
+
+function gridSize(size) {
+  console.log(size);
+  gridSpace.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+}
+
+function buildGrid(size) {
+  for (let i = 0; i < size ** 2; i++) {
+    var gridElement = document.createElement("div");
+    gridElement.classList = "grid-element";
+    gridSpace.appendChild(gridElement);
+    gridElement.addEventListener("mouseover", changeColor);
+  }
+}
+
+function changeColor(c) {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  c.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+function resizeGrid() {
+  var size = document.getElementById("size").value;
+  size = parseInt(size);
+  if (size < 1 || size > 100 || Number.isNaN(size)) {
+    alert("Try Again");
+  } else {
+    cleanHouse();
+    gridSize(size);
+    buildGrid(size);
+  }
+}
+
+function cleanHouse() {
+  while (gridSpace.firstChild) {
+    gridSpace.removeChild(gridSpace.firstChild);
+  }
 }
